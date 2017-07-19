bbf548b0c8d2c61fe88aacf61d16eef82674286f
Diljeet Singh <jeetds1994@gmail.com>
2017-07-19 09:53:14 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function iterativeLog(arr){
+  arr.forEach((element, index, array) => {
+    console.log(`${index}: ${element}`);
+  })
+}
+
+function iterate(x){
+  var arr = ["hello"]
+  arr.forEach(x);
+  return arr
+}
+function doToArray(arr, x){
+  arr.forEach(x);
+  return arr
+}
